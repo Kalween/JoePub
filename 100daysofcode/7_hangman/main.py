b1d740f74a1e52c5e741f6eeef60d6d0c4db2ddf
Kalween <kalin.joel@gmail.com>
2024-02-22 18:19:43 +0000

    Todo lists

---
+++ 100daysofcode/7_hangman/main.py
@@ -0,0 +1,7 @@
+word_list = ['bee','tennis']
+
+#TODO 1 Randomly chose a word from the list and assign it to a variable called chosen_word
+
+#TODO 2 Ask the user to guess a letter and assign their answer to a var called guess .. Make it lowecase
+
+#TODO 3 Check if the letter the user guessed (guess) is one of the letters inthe chosen_word
