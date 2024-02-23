e545c45423c171e8662848fba24e7ac884ca9240
Kalween <kalin.joel@gmail.com>
2024-02-23 22:56:25 +0000

    next part of hangman

--- 100daysofcode/7_hangman/main.py
+++ 100daysofcode/7_hangman/main.py
@@ -5,16 +5,18 @@ word_list = ['bee','tennis']
 chosen_word = random.choice(word_list)
 print(f"pst the chosen word is {chosen_word}")
 #TODO 1 Create an empty list called display.  # for each letter in the chosen_word, add a "_" to the diplay.
+display = []
+for i in chosen_word:
+    display.append("_")
 
-
+print(" ".join(display))
 guess = input("Make a guess : ").lower()
 
 #TODO 2 Lop through each positiuon in the chosen_word: # if the letter at that position matches 'guess' then revel that letter in the display at that position. 
-
-
+count = 0
 for letter in chosen_word:
     if letter == guess:
-        print("yes!")
+        chosen_word[count] = letter
     else:
         print("no")
 
