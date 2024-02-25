bec55a61c71fb842fa3d2d0cc52d3f55e22d6214
Kalween <kalin.joel@gmail.com>
2024-02-25 20:26:22 +0000

    made two functions for cyper and decypher.

--- 100daysofcode/8_caecer_cipher/main.py
+++ 100daysofcode/8_caecer_cipher/main.py
@@ -2,9 +2,26 @@ import string
 
 alphabet_list = list(string.ascii_lowercase)
 #print(alphabet_list.index("g"))
-def encrypt(text,shift):
-    pass
+shifted_list = []
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+def encrypt(text,shift):
+    for i in text:
+        shift_value = alphabet_list.index(i)+shift
+        if alphabet_list.index(i)+shift > 25:
+            shift_value = alphabet_list.index(i)+shift -25 
+        shifted_list.append(alphabet_list[shift_value])
+    print(shifted_list)
+
+def decrypt(text,shift):
+    for i in text:
+        shift_value = alphabet_list.index(i)-shift
+        if alphabet_list.index(i)+shift < 25:
+            shift_value = alphabet_list.index(i)+shift +25 
+        shifted_list.append(alphabet_list[shift_value])
+    print(shifted_list)
+
+#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+decrypt(text,shift)
