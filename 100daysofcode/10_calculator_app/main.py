e7ca49ecf31fab6c3bb440c79ae5dc0abf950b90
Kalween <kalin.joel@gmail.com>
2024-03-20 14:45:45 +0000

    Updating calender with new code

--- 100daysofcode/10_calculator_app/main.py
+++ 100daysofcode/10_calculator_app/main.py
@@ -1,5 +1,6 @@
 import art
 
+
 game_on = True
 def plus(n1,n2):
     return n1+n2
@@ -9,16 +10,49 @@ def gånger(n1,n2):
     return n1*n2
 def delat(n1,n2):
     return n1//n2
+def clear(n1,n2):
+    n1 = 0
+    n2 = 0
+
+beräkningar = {
+    '+': plus,
+    '-': minus,
+    '*': gånger,
+    '/': delat,
+    'c': clear
+}
 
 print(art.logo)
-utfall = 0 
+while game_on is True:
+    utfall = 0 
+
+    if utfall == 0:
+        
+        n1 = int(input('First number: '))
+        for symbol in beräkningar:
+            print(symbol)
+        symbol = input('Pick a calculation symbol from above : ')
+        n2 = int(input('Second number: '))
+        answer = beräkningar[f'{symbol}'](n1,n2)
+    elif func == 'clear':
+        n1 = 0  
+        n2 = 0
+        utfall == 0
+        print(art.logo)
+    elif func == 'quit':
+        game_on = False 
+    else:
+        print(f'{n1} {symbol} {n2} = {answer}')
+        symbol = input('Pick a calculation symbol from above : ')
+        n2 = int(input(f"{symbol} number: ")) 
+exit()
 while game_on is True:
 
     if utfall == 0:
         print(utfall)
-        n1 = int(input("Input number: "))
+        n1 = int(input("Input first number: "))
         func = input(" + , - , / , * , clear , quit: ")
-        n2 = int(input("Input number: "))
+        n2 = int(input("Input second number: "))
         if func == '+':
             #plus(n1,n2)
             utfall = plus(n1,n2)
