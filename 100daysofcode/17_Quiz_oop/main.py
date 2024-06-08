8a12a713630b0b70f2302a9e3876cb7b20a74652
Kalween <kalin.joel@gmail.com>
2024-06-08 23:46:27 +0200

    update

--- 100daysofcode/17_Quiz_oop/main.py
+++ 100daysofcode/17_Quiz_oop/main.py
@@ -1,4 +1,10 @@
-class Question:
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
+from question_model import Question
+from data import question_data
+
+
+questions = []
+for i in question_data:
+    question_text = i["text"]
+    question_answer = i["questions"]
+    new_question = Question(question_text, question_answer)
+    questions.append(new_question)
---
+++ 100daysofcode/17_Quiz_oop/quiz_brain.py
@@ -0,0 +1,6 @@
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
