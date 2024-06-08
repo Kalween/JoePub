2b7923465f5b95bb5cbe04c71d895d88abcedf4b
Joel <kalin.joel@gmail.com>
2024-06-09 00:28:58 +0200

    Finished quiz_brain

--- 100daysofcode/17_Quiz_oop/main.py
+++ 100daysofcode/17_Quiz_oop/main.py
@@ -1,10 +1,18 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 
 questions = []
 for i in question_data:
     question_text = i["text"]
-    question_answer = i["questions"]
+    question_answer = i["answer"]
     new_question = Question(question_text, question_answer)
     questions.append(new_question)
+
+quiz = QuizBrain(questions)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print(f"Quiz completed.\nYour final score was: {quiz.score}/{len(questions)}")
