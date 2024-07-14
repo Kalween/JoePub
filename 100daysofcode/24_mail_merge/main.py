8efdcfccea73a0ecd0fdadba1dbcbc0683b4fd58
Joel <kalin.joel@gmail.com>
2024-07-14 23:40:04 +0200

    uploaded starting files

--- 100daysofcode/24_mail_merge/main.py
+++ 100daysofcode/24_mail_merge/main.py
@@ -1,6 +1,15 @@
-with open("my_file.txt", mode='r') as file:
-    content = file.read()
-    print(content)
+# with open("my_file.txt", mode='r') as file:
+#     content = file.read()
+#     print(content)
 
 # with open("my_file.txt", mode="w") as file:
 #     file.write("\nTest new test")
+
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
