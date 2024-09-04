66a56889b886783d4eb1bd93c6092f23390d2b21
Joel <kalin.joel@gmail.com>
2024-09-04 19:59:39 +0200

    getting started with pandas

--- 100daysofcode/25_state_game/main.py
+++ 100daysofcode/25_state_game/main.py
@@ -1,9 +1,35 @@
-import csv
+import pandas
+import math
 
-with open('weather_data.csv') as file:
-    data = csv.reader(file)
-    temperatures = []
-    for row in data:
-        if row[1] != 'temp':
-            temperatures.append(int(row[1]))
-    print(temperatures)
+# data = pandas.read_csv("weather_data.csv")
+# print(type(data["temp"]))
+# data_dict = data.to_dict()
+
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+
+
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# #Get Data in columns
+# print(data["condition"])
+# print(data.condition)
+
+#Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp[0]
+# monday_temp_F = monday_temp * 9/5 +32
+# print(monday_temp_F)
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores":  [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
