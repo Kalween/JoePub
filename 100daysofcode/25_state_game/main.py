40cb62df2aef386d6ee93e6b8c27579c1715015e
kalween <kalin.joel@gmail.com>
2024-10-17 20:24:34 +0200

    update on day 25

--- 100daysofcode/25_state_game/main.py
+++ 100daysofcode/25_state_game/main.py
@@ -1,35 +1,10 @@
-import pandas
-import math
+import turtle
 
-# data = pandas.read_csv("weather_data.csv")
-# print(type(data["temp"]))
-# data_dict = data.to_dict()
+screen = turtle.Screen()
+screen.title("U.S: States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
 
-# print(data_dict)
+turtle.shape(image)
 
-# temp_list = data["temp"].to_list()
-
-
-# print(data["temp"].mean())
-# print(data["temp"].max())
-
-# #Get Data in columns
-# print(data["condition"])
-# print(data.condition)
-
-#Get Data in Row
-# print(data[data.day == "Monday"])
-# print(data[data.temp == data.temp.max()])
-
-# monday = data[data.day == "Monday"]
-# monday_temp = monday.temp[0]
-# monday_temp_F = monday_temp * 9/5 +32
-# print(monday_temp_F)
-
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores":  [76, 56, 65]
-}
-
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+screen.exitonclick()
