507810ac3fffbca6102676acc686d58f50da9a73
kalween <kalin.joel@gmail.com>
2024-10-28 15:39:50 +0100

    Finished 28_Pomodoro timer.

--- 100daysofcode/28_Pomodoro/main.py
+++ 100daysofcode/28_Pomodoro/main.py
@@ -9,12 +9,18 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-CHECK = '✅'
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text=f'00:00')
+    title_bar.config(text="Timer", fg=GREEN)
+    
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
@@ -28,18 +34,21 @@ def start_timer():
     
     if reps % 8 == 0:
         count_down(long_break_sec)
+        title_bar.config(text="Break", fg=RED)
         
     #if it's the 8th rep
     elif reps % 2 == 0:
         count_down(short_break_sec)
+        title_bar.config(text="Break", fg=PINK)
 
     else:
         count_down(work_sec)
+        title_bar.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-
+    global reps
     count_min = math.floor(count / 60) 
     count_sec = count % 60
     if count_sec < 10:
@@ -47,9 +56,16 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
     if count > 0:
-        window.after(1000,count_down, count -1)
+        global timer
+        timer = window.after(1000,count_down, count -1)
     else:
         start_timer()
+        marks = ""
+        for _ in range(math.floor(reps/2)):
+            marks += '☕'
+            image_check.config(text=marks)
+            
+        
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -68,13 +84,13 @@ canvas.grid(row=1, column= 1)
 title_bar = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME,35,"bold" ))
 title_bar.grid(row=0, column=1)
 
-image_check = Label(text=CHECK, fg=GREEN, bg=YELLOW, font=(FONT_NAME,35,"bold" ))
+image_check = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME,35,"bold" ))
 image_check.grid(row=3, column=1)
 
 button_start = Button(text = 'Start', highlightthickness=0, command=start_timer)
 button_start.grid(row=2, column=0)
 
-button_rest = Button(text = 'Reset', highlightthickness=0, command='')
+button_rest = Button(text = 'Reset', highlightthickness=0, command=reset_timer)
 button_rest.grid(row=2, column=3)
 
 
