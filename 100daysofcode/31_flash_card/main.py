5f74cd3d11545ad401aa037637245c976c478fa9
Kalween <kalin.joel@gmail.com>
2024-10-31 21:08:30 +0100

    trying to fix the timer function

--- 100daysofcode/31_flash_card/main.py
+++ 100daysofcode/31_flash_card/main.py
@@ -2,18 +2,22 @@ from tkinter import *
 import pandas as pd
 import random
 BACKGROUND_COLOR = "#B1DDC6"
+
 data = pd.read_csv("./data/french_words.csv")
 
 # ---------------------------- Words to read ------------------------------- #
 words_completed = []
 def word_game():
+    flashcard_timer = 3
     random_word = random.randint(1, len(data))
     
     if random_word not in words_completed:
         print(data['French'][random_word])
+        if flashcard_timer > 0:
+            window.after(1000, flashcard_timer -1)
         print(data['English'][random_word])
         words_completed.append(random_word)
-
+    return data['French'][random_word]
 
 
 
@@ -32,14 +36,16 @@ card_back_image = PhotoImage(file="./images/card_back.png")
 
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 canvas.create_image(400,263,image=card_front_image)
+canvas.create_text(400, 150, text='Title',font=("Ariel", 40, "italic"))
+word_canvas = canvas.create_text(400, 263, text=word_game(),font=("Ariel", 60, "bold"))
 canvas.grid(row=1, column=1, padx=50, pady=50, columnspan=2)
 
 #Labels
-french_word = Label(text='test', anchor=E, borderwidth=0)
-french_word.grid(row=1, column=1, sticky='e')
 
 
-right_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, border=0) 
+
+
+right_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, border=0, command=word_game()) 
 right_button.grid(row=10, column=1, padx=10, pady=10)
 wrong_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, border=0)
 wrong_button.grid(row=10, column=2, padx=10, pady=10)
