940b922ec840d2f74d4a44ba3d88e2c247aa000d
Kalween <kalin.joel@gmail.com>
2024-11-03 19:29:37 +0100

    finished flashcards

--- 100daysofcode/31_flash_card/main.py
+++ 100daysofcode/31_flash_card/main.py
@@ -1,65 +1,70 @@
 from tkinter import *
-import pandas as pd
+import pandas
 import random
+
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
-data = pd.read_csv("./data/french_words.csv")
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
-# ---------------------------- Words to read ------------------------------- #
-words_completed = []
-# Funktion för att byta till engelska ordet efter timer
-def show_english_card(current_card):
-    canvas.itemconfig(bg_canvas, image=card_back_image)
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(word_title, text=data['English'][current_card])
 
-# Funktion för att starta nästa kort och timer
 def next_card():
-    canvas.itemconfig(bg_canvas, image=card_front_image)
-    current_card = random.randint(0, len(data) - 1)
-    
-    # Kontrollera om kortet redan är slutfört
-    if current_card not in words_completed:
-        canvas.itemconfig(card_title, text="French")
-        canvas.itemconfig(word_title, text=data['French'][current_card])
-        print(data['English'][current_card])  # Visar engelska ordet i terminalen för kontroll
-        words_completed.append(current_card)
-        
-        # Starta timer för att visa engelska efter 3 sekunder
-        window.after(3000, lambda: show_english_card(current_card))
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
-# ---------------------------- Store progress ------------------------------- #
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
-# ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
-window.config(bg=BACKGROUND_COLOR)
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-right_image = PhotoImage(file="./images/right.png")
-wrong_image = PhotoImage(file="./images/wrong.png")
-card_front_image = PhotoImage(file="./images/card_front.png")
-card_back_image = PhotoImage(file="./images/card_back.png")
+flip_timer = window.after(3000, func=flip_card)
 
-canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-bg_canvas = canvas.create_image(400,263,image=card_front_image)
-card_title = canvas.create_text(400, 150, text='Title',font=("Ariel", 40, "italic"))
-word_title = canvas.create_text(400, 263, text='Word',font=("Ariel", 60, "bold"))
-canvas.grid(row=1, column=1, padx=50, pady=50, columnspan=2)
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
-#Labels
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
 
-
-
-right_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, border=0, command=next_card) 
-right_button.grid(row=10, column=1, padx=10, pady=10)
-wrong_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COLOR, border=0, command=next_card)
-wrong_button.grid(row=10, column=2, padx=10, pady=10)
-
-
-
+next_card()
 
 window.mainloop()
+
 
