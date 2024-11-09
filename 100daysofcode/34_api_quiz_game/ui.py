0eb26d79ba46e60b8c98ee91e0fa24e934d7f3a3
Kalween <kalin.joel@gmail.com>
2024-11-09 21:18:26 +0100

    started ui

--- 100daysofcode/34_api_quiz_game/ui.py
+++ 100daysofcode/34_api_quiz_game/ui.py
@@ -1,2 +1,24 @@
+from tkinter import *
 THEME_COLOR = "#375362"
+BUTTON_TRUE = 'images/true.png'
+BUTTON_FALSE = 'images/false.png'
 
+window = Tk()
+
+window.title("Quizz App")
+window.config(background=THEME_COLOR)
+
+canvas = Canvas(width=800, height=526)
+true_img = PhotoImage(file=BUTTON_TRUE)
+false_img = PhotoImage(file=BUTTON_FALSE)
+
+button_true = Button(image=true_img, highlightthickness=0)
+button_true.grid(row=10, column=0)
+
+button_false = Button(image=false_img, highlightthickness=0)
+button_false.grid(row=10, column=1)
+
+
+
+
+window.mainloop()
