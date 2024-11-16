257d3b236a4e696d0da467cd115dc337f4d53861
Kalween <kalin.joel@gmail.com>
2024-11-16 21:27:39 +0100

    completed weather app

--- 100daysofcode/35_api_openweather/main.py
+++ 100daysofcode/35_api_openweather/main.py
@@ -5,11 +5,24 @@ url = "https://api.openweathermap.org/data/2.5/forecast?"
 
 
 parameters = {
-    "lat":122.70,
-    "lon":33.76,
-    "appid":settings.API_KEY}
+    "lat":58.706070,
+    "lon":15.767550,
+    "appid":settings.API_KEY,
+    "units":"metric",
+    "cnt":4}
 
 response = requests.get(url, params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+
+will_rain = False
+condition_codes = [hour_data["weather"][0]["id"] for hour_data in data["list"]]
+if any(i <= 700 for i in condition_codes):
+    will_train = True
+
+if will_rain:
+    print("Bring an umbrella") 
+
+
+exit()
+print(data["list"][0]["weather"][0]["id"])
